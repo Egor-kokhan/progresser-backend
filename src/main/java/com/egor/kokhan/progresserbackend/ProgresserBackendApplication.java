6d584d8784b6beb1e28398a5afd79c09496ee81f
Egor <new.egor.kokhan@gmail.com>
2023-11-20 02:17:17 +0300

    init

---
+++ src/main/java/com/egor/kokhan/progresserbackend/ProgresserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.egor.kokhan.progresserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgresserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgresserBackendApplication.class, args);
+	}
+
+}
